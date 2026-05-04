4b2a67e8ff29a67a9bf61c9d8e98333da41f9a97
Lara Anton <antonsobrecasaslara@gmail.com>
2026-05-03 21:33:27 -0300

    resolucion problema 4

--- Dsw2026Ej8/Ejercicio8/Problema3.cs
+++ Dsw2026Ej8/Ejercicio8/Problema3.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http.Headers;
 using System.Text;
 
-namespace Dsw2026Ej8
+namespace Dsw2026Ej8.Ejercicio8
 {
     internal class Problema3
     {
+        public string CompararCopias(int originalValue, Product product)
+        {
+            int copyValue = originalValue;
+            copyValue++;
+            Product copyProduct = product;
+            copyProduct.ModifyDescription("Descripcion Modificada");
+
+            return $"{originalValue} - {copyValue} - {product.Description}, Producto Copia: {copyProduct.Name}";
+        }
     }
 }
---
+++ Dsw2026Ej8/Ejercicio8/Problema4.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Ejercicio8
+{
+    internal class Problema4
+    {
+        public double CalcularPromedio(int? nota1, int? nota2, int? nota3)
+        {
+            int suma = 0;
+            int cantidadNotas = 0;
+
+            if(nota1.Value is null && nota2.Value is null && nota3.Value is null)
+                return 0.0;
+
+            if (nota1.HasValue)
+            {
+                suma += nota1.Value;
+                cantidadNotas++;
+            }
+            if (nota2.HasValue)
+            {
+                suma += nota2.Value;
+                cantidadNotas++;
+            }
+            if (nota3.HasValue)
+            {
+                suma += nota3.Value;
+                cantidadNotas++;
+            }
+
+           
+
+            return cantidadNotas > 0 ? (double)suma / cantidadNotas : 0.0;
+        }
+
+    }
+}
