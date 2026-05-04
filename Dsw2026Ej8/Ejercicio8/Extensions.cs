dd8299f58edffde22789e4f0f1704f0af347a5c7
Lara Anton <antonsobrecasaslara@gmail.com>
2026-05-04 00:46:12 -0300

    resolucion problema 6

---
+++ Dsw2026Ej8/Ejercicio8/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Ejercicio8
+{
+    internal static class Extensions
+    {
+        public static string ToProductCode(this string code)
+        {
+
+            if (code is null)
+                return "SIN-CODIGO";
+
+            return code
+                .Trim()
+                .ToUpper()
+                .Replace(" ", "-");
+
+        }
+    }
+}
